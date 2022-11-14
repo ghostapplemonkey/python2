6b9fa6f8852f036a9e79fca0b3824cda30cd721f
ghostapplemonkey <87847262+ghostapplemonkey@users.noreply.github.com>
2022-11-14 20:26:18 +0800

    Add files via upload

--- BFS/Path_finding.py
+++ BFS/Path_finding.py
@@ -1,6 +1,7 @@
-from pycat.core import Window, Sprite, Color, Point
+from pycat.core import Window, Sprite, Color, Point, KeyCode
 import random
 from bfs import Bfs
+from pycat.base.event import MouseEvent, MouseButton
 
 window = Window(width=1280, height=640) 
 
@@ -9,6 +10,88 @@ def get_cell(position : Point):
         if cell.distance_to(position) < 2:
             return cell
     return None
+
+class Runningbox(Sprite):
+    def on_create(self):
+        self.scale = 30
+        self.index = 1
+        self.running = False
+        self.time = 0
+        self.layer = 1000
+    def on_update(self, dt):
+        if self.running == True:
+            self.point_toward_sprite(solveman.path[self.index])
+            self.move_forward(10)
+            if self.distance_to(solveman.path[self.index]) <= 20:
+                self.index += 1
+            if self.index == len(solveman.path):
+                self.running = False
+
+runningbox = window.create_sprite(Runningbox)
+
+class SolveMan(Sprite):
+    def on_create(self):
+        self.bfs = Bfs()
+        self.start : Sprite
+        self.end : Sprite
+        self.result : bool
+        self.time = 0
+        self.solving = False
+        self.path : list[Sprite]
+    def on_update(self, dt):
+        if window.is_key_pressed(KeyCode.B):
+            self.solve(0)
+            self.display_path()
+        if window.is_key_pressed(KeyCode.D):
+            self.solve(1)
+            self.display_path()
+        self.time += dt
+        if window.is_key_pressed(KeyCode.N):
+            self.slowsolving(sw=0)
+            self.solving = True
+        if window.is_key_pressed(KeyCode.F):
+            self.slowsolving(sw=1)
+            self.solving = True
+        if self.solving == True:
+            if self.time > 0.05:
+                self.result = self.bfs.step()
+                self.time = 0
+                if self.bfs.v != self.bfs.start and self.bfs.v != self.bfs.end:
+                    self.bfs.v.color = Color.YELLOW
+                if self.bfs.v == self.bfs.end:
+                    self.solving = False
+                    self.display_path()
+        if window.is_key_pressed(KeyCode.P):
+            self.solve(0)
+            self.path = self.bfs.path()
+            runningbox.position = self.start.position
+            runningbox.running = True
+                    
+
+
+    def slowsolving(self, sw):
+        self.setup_cells()
+        self.bfs.setup(start=self.start, end=self.end, sw=sw)
+        
+    def solve(self, sw):
+        self.setup_cells()
+        self.bfs.setup(start=self.start, end=self.end, sw=sw)
+        self.result = self.bfs.solve()
+    def setup_cells(self):
+        for cell in window.get_all_sprites():
+            if cell.color != Color.RED and cell.color != Color.GREEN:
+                cell.clear_tags()
+                cell.color = Color.WHITE
+        
+        for cell in window.get_sprites_with_tag("start"):
+            self.start = cell
+        for cell in window.get_sprites_with_tag("end"):
+            self.end = cell
+    def display_path(self):
+        if self.result == True:
+            for i in self.bfs.path():
+                if i!=self.start and i!=self.end:
+                    i.color = Color.AZURE
 
 
 class Cell(Sprite):
@@ -21,29 +104,37 @@ class Cell(Sprite):
         return neighbors
     def __str__(self):
         return str(int((self.x-32)/64))+","+str(int((self.y-32)/64))
+    def on_click(self, mouse_event: MouseEvent):
+        if(mouse_event.button == MouseButton.LEFT and self.image == "ground.png"):
+            for cell in window.get_all_sprites():
+                if cell.color != Color.GREEN:
+                    cell.clear_tags()
+                    cell.color = Color.WHITE
+            self.add_tag("end")
+            self.color = Color.RED
+        if(mouse_event.button == MouseButton.RIGHT and self.image == "ground.png"):
+            for cell in window.get_all_sprites():
+                if cell.color != Color.RED:
+                    cell.clear_tags()
+                    cell.color = Color.WHITE
+            self.add_tag("start")
+            self.color = Color.GREEN
+        
 
 for i in range(0,640,64):
     for j in range(0,1280,64):
         k=random.random()
-        if k>0.4:
+        if k>0:
             cell = window.create_sprite(Cell,x=j+32,y=i+32)
             cell.image = "ground.png"
         else:
             cell = window.create_sprite(Cell,x=j+32,y=i+32)
             cell.image = "block.png"
 
-start = get_cell(Point(32+64*0,32+64*0))
-end = get_cell(Point(32+64*12,32+64*8))
-start.image = "ground.png"
-end.image = "ground.png"
-start.color = Color.GREEN
-end.color = Color.RED
+solveman = window.create_sprite(SolveMan)
 
-bfs = Bfs()
-print(bfs.solve(start=start, end=end))
-for i in bfs.path(start=start, end=end):
-    if i!=start and i!=end:
-        i.color = Color.AZURE
+
+
 
 window.run()
 
--- BFS/bfs.py
+++ BFS/bfs.py
@@ -1,29 +1,46 @@
 class Bfs():
-    def solve(self, start ,end):
+    def setup(self, start ,end, sw):
         self.queue = [start]
         self.explored = [start]
         self.parent = {}
+        self.sw = sw
+        self.start = start
+        self.end = end
+        self.result = False
+    def solve(self):
         while self.queue != []:
-            v = self.queue.pop(0)
-            if v == end:
+            self.result = self.step()
+            if self.result == True:
                 return True
-            for n in v.get_neighbors():
+        return False
+    def step(self):
+        self.v = self.queue.pop(0)
+        if self.v == self.end:
+            return True
+        if self.sw == 0:
+            for n in self.v.get_neighbors():
                 if n not in self.explored:
-                    self.parent[n] = v
+                    self.parent[n] = self.v
                     self.queue.append(n)
                     self.explored.append(n)
-            print(v)
-            print(' ~ '.join([str(i) for i in self.queue]))
-    def path(self, start ,end):
-        v = end
-        path = [v]
-        while v!=start:
-            v = self.parent[v]
-            path.append(v)
+        else:
+            for n in self.v.get_neighbors():
+                if n not in self.explored:
+                    self.parent[n] = self.v
+                    self.queue.insert(0, n)
+                    self.explored.append(n)
+        return False
+        print(self.v)
+        print(' ~ '.join([str(i) for i in self.queue]))
+    def path(self):
+        self.v = self.end
+        path = [self.v]
+        while self.v!=self.start:
+            self.v = self.parent[self.v]
+            path.insert(0,self.v)
         print(' - '.join([str(i) for i in path]))
         return path
             
 
 
  
-        return False
